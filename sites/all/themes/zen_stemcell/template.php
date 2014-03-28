f29ae0b23a293056de44ee7648ff77019fa522eb
Dave Rosen <dmrosen@qplot.com>
2014-03-28 10:42:31 -0400

    added article labels to articles

--- sites/all/themes/zen_stemcell/template.php
+++ sites/all/themes/zen_stemcell/template.php
@@ -55,11 +55,21 @@ function STARTERKIT_preprocess_html(&$variables, $hook) {
 function zen_stemcell_preprocess_page(&$variables, $hook) {
   if (!empty($variables['node'])) {
     $node = &$variables['node'];
+    // dsm($node);
     if ($node->type == 'article') {
-      $date = format_date($node->created, 'custom', 'F j, Y');
+      $date = format_date($node->created, 'custom', 'F , Y');
+      $label = '';
+      if (!empty($node->field_label['und'][0]['tid'])) {
+        $tid = $node->field_label['und'][0]['tid'];
+        if ($term = taxonomy_term_load($tid)) {
+          // dsm($term);
+          $label = l($term->name, "taxonomy/term/$tid");
+        };
+      }
       $variables['title_prefix'] = array(
         '#type' => 'markup',
-        '#markup' => '<div class="article-date">' . $date . '</div>',
+        '#markup' => '<div class="article-date">' . $date . '</div>' . 
+        '<span>' . $label . '</span>',
       );
     }
   }
@@ -151,4 +161,95 @@ function zen_stemcell_form_alter(&$form, &$form_state, $form_id) {
 	}
   
 }
+
+/**
+ * Return a themed breadcrumb trail.
+ *
+ * @param $variables
+ *   - title: An optional string to be used as a navigational heading to give
+ *     context for breadcrumb links to screen-reader users.
+ *   - title_attributes_array: Array of HTML attributes for the title. It is
+ *     flattened into a string within the theme function.
+ *   - breadcrumb: An array containing the breadcrumb links.
+ * @return
+ *   A string containing the breadcrumb output.
+ */
+function zen_stemcell_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+  $output = '';
+
+  // Determine if we are to display the breadcrumb.
+  $show_breadcrumb = theme_get_setting('zen_breadcrumb');
+  if ($show_breadcrumb == 'yes' || $show_breadcrumb == 'admin' && arg(0) == 'admin') {
+
+    // Optionally get rid of the homepage link.
+    $show_breadcrumb_home = theme_get_setting('zen_breadcrumb_home');
+    if (!$show_breadcrumb_home) {
+      array_shift($breadcrumb);
+    }
+
+    // Return the breadcrumb with separators.
+    if (!empty($breadcrumb)) {
+      $breadcrumb_separator = filter_xss_admin(theme_get_setting('zen_breadcrumb_separator'));
+      $trailing_separator = $title = '';
+      if (theme_get_setting('zen_breadcrumb_title')) {
+        $item = menu_get_item();
+        if (!empty($item['tab_parent'])) {
+          // If we are on a non-default tab, use the tab's title.
+          $breadcrumb[] = check_plain($item['title']);
+        }
+        else {
+          $breadcrumb[] = drupal_get_title();
+        }
+      }
+      elseif (theme_get_setting('zen_breadcrumb_trailing')) {
+        $trailing_separator = $breadcrumb_separator;
+      }
+
+      // Fang's code to display node taxnomy 
+      $args = arg();
+      if ((count($args) > 1) && ($args[0] == 'node')) {
+        $node_id = $args[1];
+        $node = node_load($node_id);
+        if ($node->type == 'article') {
+          if (!empty($node->field_tags['und'][0]['tid'])) {
+            $tag_id = $node->field_tags['und'][0]['tid'];
+
+            // load this term
+            $term = taxonomy_term_load($tag_id);
+            $title = $term->name;
+
+            //$link = "journal_club_article_scans";
+            // $breadcrumb[] = l($title, $link);
+
+            $breadcrumb[] = $title;
+            
+            // dsm($tag_id);
+          }
+        }
+        // dsm($node);
+      }
+
+      // Provide a navigational heading to give context for breadcrumb links to
+      // screen-reader users.
+      if (empty($variables['title'])) {
+        $variables['title'] = t('You are here');
+      }
+      // Unless overridden by a preprocess function, make the heading invisible.
+      if (!isset($variables['title_attributes_array']['class'])) {
+        $variables['title_attributes_array']['class'][] = 'element-invisible';
+      }
+
+      // Build the breadcrumb trail.
+      $output = '<nav class="breadcrumb" role="navigation">';
+      $output .= '<h2' . drupal_attributes($variables['title_attributes_array']) . '>' . $variables['title'] . '</h2>';
+      $output .= '<ol><li>' . implode($breadcrumb_separator . '</li><li>', $breadcrumb) . $trailing_separator . '</li></ol>';
+      $output .= '</nav>';
+    }
+  }
+
+  return $output;
+}
+
+
 ?>
